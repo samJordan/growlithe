dea93ce69f7dfd18c52ed8d71f8cb4a362a5cd83
hewhocodes <sjordan@mail.umw.edu>
2012-11-17 18:14:09 -0500

    Update src/GUIScreens.java

--- src/GUIScreens.java
+++ src/GUIScreens.java
@@ -1,8 +1,8 @@
-
 public class GUIScreens {
 
 	public static void main(String[] args){
 		System.out.println("HELLO TO THE GODDAMN WORLD");
+		System.out.println("MY TEST TO TEST THAT THE TEST IS TESTING CORRECTLY AS A TEST");
 	}
 	
 }
